3d9adac8dc618847a52234a4a11feaf2ea0fa401
oscabaho <osbaho86@gmail.com>
2025-11-24 19:35:04 -0500

    Add Variable.js with basic variable declarations
    Introduces Variable.js, which demonstrates variable declarations, assignments, and console output for personal information and a numeric calculation.

---
+++ Variable.js
@@ -0,0 +1,19 @@
+let num1 = 5;
+
+let resultadoNum1 = num1 ** 3;
+
+console.log(resultadoNum1);
+
+let nombre = "Oscar";
+
+let apellido = "Barbosa";
+
+let edad;
+
+edad = 39;
+
+let genero = "Masculino";
+
+let estadoCivil = "Union Libre";
+
+console.log(`Nombre: ${nombre}, Apellido: ${apellido}, Edad: ${edad}, Género: ${genero}, Estado Civil: ${estadoCivil}`);
